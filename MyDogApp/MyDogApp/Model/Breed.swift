19e1f2afd459b582e197307006c4bbf01ba5d799
Sarah Arshad <sarshad1@yahoo.co.uk>
2023-01-31 12:35:32 +0000

    added comments

--- MyDogApp/MyDogApp/Model/Breed.swift
+++ MyDogApp/MyDogApp/Model/Breed.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// codable means your struct can be converted from JSON or converted to JSON
+// codable means your struct can be converted from JSON or converted to JSON. Struct to map the data to once we get it back from the url.
 struct Breed: Codable {
     let id: Int
     let name: String?
--- MyDogApp/MyDogApp/Service/FetchService.swift
+++ MyDogApp/MyDogApp/Service/FetchService.swift
@@ -19,17 +19,19 @@ class FetchService {
         let request = URLRequest(url: url)
         
         // Creates a data task that retrieves the contents of the url and delivers it's results(the server's response, data and errors) to a call back function/completon closure that gets executed once the request completes.
-        let dataTask = networkSession.dataTask(with: request) { data, response, error in
+        let dataTask = networkSession.dataTask(with: request, completionHandler: { data, response, error in
             guard let data = data else {
                 return
             }
-            print(data)
-            //why do you need to specify .self?
+            //We need to convert the JSON data to the Swift struct. We do this using JSON decoder.
+            //Why do you need to specify .self? [Breed].self is the name of the type. It's Swift's way of referring to the [Breed] type itself and not an instance of it. We're not saying "create a new one", but instead specifying it as a parameter to the decoding so JSONDecoder knows what to convert the JSON to.
             let breeds = try? self.decoder.decode([Breed].self, from: data)
+            print(breeds)
             
+            // if the completion handler returns nil breeds then return an empty array.
             completionHandler(breeds ?? [])
-        }
-        // The task is created in a suspended state so to make it actually make a request we have to resume the task.
+        })
+        // The task is created in a suspended state so to make it actually fire a request we have to resume the task.
         dataTask.resume()
         
 //        return [ .init(id: 1, name: "Labrador"), .init(id: 2, name: "Cockapoo")]
